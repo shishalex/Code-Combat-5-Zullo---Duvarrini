1cccae8218acc8d3920aa919917bb5f47f95dad4
Loris <loris.duvarrini@gmail.com>
2026-01-14 10:04:05 +0100

    Add files via upload

--- Object Code Combat 5/view.py
+++ Object Code Combat 5/view.py
@@ -8,14 +8,22 @@ def show_welcome():
 
 
 def show_validation_error(component, message):
-    print(f"Creazione '{component}' fallita: {Fore.RED}{message}")
+    print(f"Si è verificato un errore con '{component}': {Fore.RED}{message}")
+
 
 def show_default_weapon(player_name, default_weapon_name):
     print(f"⚔️ {player_name} usa: {Fore.LIGHTBLUE_EX}{default_weapon_name}{Fore.RESET}.")
 
-def show_initial_stats(p1_name, p2_name, p1_hp, p2_hp, p1_max_hp, p2_max_hp, p1_str, p2_str, p1_dex, p2_dex):
-    print(f"{p1_name} {Fore.LIGHTBLUE_EX}(HP: {p1_hp}/{p1_max_hp}, STR: {p1_str}, DEX: {p1_dex})\n"
-          f"{Fore.RESET}{p2_name} {Fore.LIGHTBLUE_EX}(HP: {p2_hp}/{p2_max_hp}, STR: {p2_str}, DEX: {p2_dex})")
+
+def show_initial_stats(p1_stats: dict, p2_stats: dict):
+    print(
+        f"{p1_stats["name"]} {Fore.LIGHTBLUE_EX}(HP: {p1_stats["health"]}/{p1_stats["max_health"]}, STR: {p1_stats["strength"]}, DEX: {p1_stats["dexterity"]})\n"
+        f"{Fore.RESET}{p2_stats["name"]} {Fore.LIGHTBLUE_EX}(HP: {p2_stats["health"]}/{p2_stats["max_health"]}, STR: {p2_stats["strength"]}, DEX: {p2_stats["dexterity"]})\n")
+
+
+def show_weapon_equip(player_name, weapon):
+    print(f"⚔️ {player_name} equipaggia: {Fore.YELLOW}{weapon} {Fore.RESET}⚔️")
+
 
 def show_turn_header(turn_number):
     print(f"{Style.BRIGHT}{Fore.YELLOW}--- Turno {turn_number} ---")
@@ -32,7 +40,8 @@ def show_action_failure(player_name, action_name, reason):
 
 
 def show_potion_success(player_name, effect_desc, current_hp_msg):
-    print(f"✨ {player_name} usa la pozione: {Fore.LIGHTYELLOW_EX}{effect_desc}{Fore.RESET} -> {player_name} HP: {Fore.LIGHTYELLOW_EX}{current_hp_msg}")
+    print(
+        f"✨ {player_name} usa la pozione: {Fore.LIGHTYELLOW_EX}{effect_desc}{Fore.RESET} -> {player_name} HP: {Fore.LIGHTYELLOW_EX}{current_hp_msg}")
 
 
 def show_attack_result(attacker_name, defender_name, damage, eff_stat):
